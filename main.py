e6fa471d2190e5c2d9b770062d0de52af6d33443
dd-dos <dungphamdang99@gmail.com>
2021-03-02 10:32:32 +0700

    fast commit

--- main.py
+++ main.py
@@ -100,9 +100,9 @@ def evaluation(net, valloader, criterion, valset, checkpoint, bitwidths):
 
             # print statistics
             running_loss += loss.item()
-            if i % 100 == 1:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 100))
+            if i % 10 == 9:    # print every 2000 mini-batches
+                print('current loss: %.3f' %
+                    (running_loss / 640))
                 running_loss = 0.0
         
         average_loss = running_loss/num_samples
