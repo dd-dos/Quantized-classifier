0b5dde53b7f1f84b78849323363492cc7837dd1e
dd-dos <dungphamdang99@gmail.com>
2021-03-03 16:41:31 +0700

    fast commit

--- main.py
+++ main.py
@@ -134,14 +134,14 @@ def evaluation(args, net, valloader, criterion, valset, checkpoint, bitwidths):
 
 
 def test_qtmodel(checkpoint, split='val'):
-    net_fp32 = QuantizableMobileNetV2(num_classes=10)
+    net_fp32 = mobilenet_v2(num_classes=10)
     net_fp32.train()
     net_fp32.qconfig = torch.quantization.get_default_qat_qconfig('fbgemm') #fbgemm for pc; qnnpack for mobile
     torch.backends.quantized.engine='fbgemm'
     prepared_net_fp32 = torch.quantization.prepare(net_fp32, inplace=True)
-    # prepared_net_fp32.load_state_dict(torch.load(checkpoint))
+    prepared_net_fp32.load_state_dict(torch.load("/content/drive/MyDrive/training/Quantized-classifier/fp32_best.pth"))
     net_int8 = torch.quantization.convert(prepared_net_fp32.cpu().eval())
-    net_int8.load_state_dict(torch.load(checkpoint))
+    # net_int8.load_state_dict(torch.load(checkpoint))
     # print(torch.load(checkpoint))
     net_int8.eval()
 
