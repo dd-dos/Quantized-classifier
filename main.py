a761ea1da394bebd356ebe3234b40a21b3e96e12
dd-dos <dungphamdang99@gmail.com>
2021-03-03 16:23:25 +0700

    fast commit

--- main.py
+++ main.py
@@ -134,13 +134,14 @@ def evaluation(args, net, valloader, criterion, valset, checkpoint, bitwidths):
 
 
 def test_qtmodel(checkpoint, split='val'):
-    net_fp32 = mobilenet_v2(num_classes=10)
-    net_fp32.train()
-    net_fp32.qconfig = torch.quantization.get_default_qat_qconfig('fbgemm') #fbgemm for pc; qnnpack for mobile
-    torch.backends.quantized.engine='fbgemm'
-    prepared_net_fp32 = torch.quantization.prepare_qat(net_fp32)
-    # prepared_net_fp32.load_state_dict(torch.load(checkpoint))
-    net_int8 = torch.quantization.convert(prepared_net_fp32.cpu().eval())
+    # net_fp32 = mobilenet_v2(num_classes=10)
+    # net_fp32.train()
+    # net_fp32.qconfig = torch.quantization.get_default_qat_qconfig('fbgemm') #fbgemm for pc; qnnpack for mobile
+    # torch.backends.quantized.engine='fbgemm'
+    # prepared_net_fp32 = torch.quantization.prepare_qat(net_fp32)
+    # # prepared_net_fp32.load_state_dict(torch.load(checkpoint))
+    # net_int8 = torch.quantization.convert(prepared_net_fp32.cpu().eval())
+    net_int8 = mobilenet_v2(num_classes=10, quantize=True)
     net_int8.load_state_dict(torch.load(checkpoint))
     # print(torch.load(checkpoint))
     for name, param in net_int8.named_parameters():
