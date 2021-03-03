e1aa3190a774710e49ca17b237e55e1e9d92534c
dd-dos <dungphamdang99@gmail.com>
2021-03-03 15:41:14 +0700

    fast commit

--- main.py
+++ main.py
@@ -143,8 +143,8 @@ def test_qtmodel(checkpoint, split='val'):
     net_int8 = torch.quantization.convert(prepared_net_fp32.cpu().eval())
     # net_int8.load_state_dict(torch.load(checkpoint))
     # print(torch.load(checkpoint))
-    # for name, param in net_int8.named_parameters():
-    #     print("name: {} - param: {} \n".format(name, param))
+    for name, param in net_int8.named_parameters():
+        print("name: {} - param: {} \n".format(name, param))
     net_int8.eval()
 
     transform = transforms.Compose(
