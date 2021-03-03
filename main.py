a5efa4cb8a6f6a2e846b583a51e3d00df3b597a8
dd-dos <dungphamdang99@gmail.com>
2021-03-03 15:28:35 +0700

    fast commit

--- main.py
+++ main.py
@@ -232,7 +232,7 @@ def argparser():
     return args
 
 if __name__=="__main__":
-    args = argparser()
-    train(args)
-    # print(test_qtmodel("/content/drive/MyDrive/training/Quantized-classifier/int8_best.pth"))
+    # args = argparser()
+    # train(args)
+    print(test_qtmodel("/content/drive/MyDrive/training/Quantized-classifier/int8_best.pth"))
     # print(test_fp32_model("/content/drive/MyDrive/training/Quantized-classifier/fp32_best.pth"))
