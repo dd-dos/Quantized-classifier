dbcda0f56bf73fe1c7064ea65256f1de5a0b2eea
dd-dos <dungphamdang99@gmail.com>
2021-03-03 16:50:17 +0700

    fast commit

--- main.py
+++ main.py
@@ -239,5 +239,5 @@ def argparser():
 if __name__=="__main__":
     # args = argparser()
     # train(args)
-    # print(test_qtmodel("/content/drive/MyDrive/training/Quantized-classifier/int8_best.pth", split='val'))
-    print(test_fp32_model("/content/drive/MyDrive/training/Quantized-classifier/fp32_best.pth", split='val'))
+    print(test_qtmodel("/content/drive/MyDrive/training/Quantized-classifier/int8_best.pth", split='val'))
+    # print(test_fp32_model("/content/drive/MyDrive/training/Quantized-classifier/fp32_best.pth", split='val'))
