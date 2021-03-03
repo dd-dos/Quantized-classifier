201aa9d6eeaf5c0478d94b2855ba6b8c3abc6643
dd-dos <dungphamdang99@gmail.com>
2021-03-03 09:59:45 +0700

    fast commit

--- main.py
+++ main.py
@@ -150,6 +150,10 @@ def test_qtmodel(checkpoint):
 def test_fp32_model(checkpoint):
     net_fp32 = mobilenet_v2(num_classes=10)
     net_fp32.load_state_dict(torch.load(checkpoint))
+    net_fp32.train()
+    net_fp32.qconfig = torch.quantization.get_default_qat_qconfig('fbgemm') #fbgemm for pc; qnnpack for mobile
+    torch.backends.quantized.engine='fbgemm'
+    net_fp32.eval()
     transform = transforms.Compose(
             [transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
