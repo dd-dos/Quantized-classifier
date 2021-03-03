973df192a8f81e54572053fcbe2b8153872f43a0
dd-dos <dungphamdang99@gmail.com>
2021-03-03 15:16:00 +0700

    fast commit

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,3 @@
 python main.py --batch_size 64 \
                --num_workers 8 \
-               --cp "/content/drive/MyDrive/training/Quantized-classifier"
+               --cp "/content/drive/MyDrive/training/Quantized-classifier/Test"
